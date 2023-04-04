eeae115ec3714eb55782684e92dcef48020084b9
Ravi Singh <ravisingh007ravi@gmail.com>
2023-04-04 23:25:38 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
-import {
-  LogIn, SignUp, Home, DataProvider, UpdateBlogs, NotFoundPage,
-  Navbar, About, Contact, CreatePost, DetailsViewPost, DeletedPost
-} from './components/AllComponents';
 
 
-
-const PriveRoute = ({ isAuthentication, ...props }) => {
-  return isAuthentication ?
-    <> <Navbar />
-      <Outlet />
-    </>
-    :
-    <Navigate replace to='/LogIn' />
-}
 
 function App() {
-
-  const [isAuthentication, isUserAuthentication] = useState(false);
-
   return (
-    <DataProvider>
-      <BrowserRouter>
-        <Routes>
-          
-          <Route path='SignUp' element={<SignUp />} />
-          <Route path='LogIn' element={<LogIn isUserAuthentication={isUserAuthentication} />} />
-
-          <Route path='/' element={<PriveRoute isAuthentication={isAuthentication} />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/About' element={<About />} />
-            <Route path='/Contact' element={<Contact />} />
-            <Route path='/create' element={<CreatePost />} />
-            <Route path='/DetailsViewPost/:id' element={<DetailsViewPost />} />
-            <Route path='/UpdateBlogs/:id' element={<UpdateBlogs />} />
-            <Route path='/DeletedPost/:id' element={<DeletedPost />} />
-          </Route>
-            <Route path='/*' element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </DataProvider>
-  )
+    <div className="App">
+      <h1>Ravi Singh</h1>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
